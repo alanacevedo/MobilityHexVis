17c36ce536a7346ce7145fe7c09ec7fbd9bebede
alanacevedo <michael.acevedo@ug.uchile.cl>
2024-02-20 13:44:20 -0300

    leaflet + d3 setup

--- displayMap.js
+++ displayMap.js
@@ -1,41 +1,50 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import * as d3Tile from "https://cdn.jsdelivr.net/npm/d3-tile@1/+esm"
+import * as L from 'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/+esm'
+
+// https://bost.ocks.org/mike/leaflet/
+// https://d3-graph-gallery.com/graph/bubblemap_leaflet_basic.html
 
 import { accessToken } from "./static.js"
 const url = (x, y, z) => `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/${z}/${x}/${y}?access_token=${accessToken}`
 
-const height = 600
-const width = 600
+const INITIAL_CENTER = [-33.471258, -70.646552]
+const INITIAL_ZOOM = 11
+const MAX_ZOOM = 12
 
-const svg = d3.select("#canvas")
+const map = L
+  .map('map')
+  .setView(INITIAL_CENTER, INITIAL_ZOOM); 
 
-const tile = d3Tile.tile()
-      .extent([[0, 0], [width, height]])
-      .tileSize(512)
-      .clampX(false);
+L.tileLayer(
+      `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${accessToken}`,
+      {maxZoom: MAX_ZOOM}
+).addTo(map)
 
-const zoom = d3.zoom()
-    .scaleExtent([1 << 8, 1 << 22])
-    .extent([[0, 0], [width, height]])
-    .on("zoom", ({transform}) => zoomed(transform));
+L.svg().addTo(map)
 
-let image = svg.append("g")
-.attr("pointer-events", "none")
-.selectAll("image");
+const markers = [
+      {lat: -33.518213, lon: -70.742712}, 
+      {lat:-33.442615, lon: -70.568191}, 
+]
 
-svg
-      .call(zoom)
-      .call(zoom.transform, d3.zoomIdentity
-        .translate(width >> 1, height >> 1)
-        .scale(1 << 12));
+d3.select("#map")
+ .select("svg")
+ .selectAll("circles")
+ .data(markers)
+ .join("circle")
+ .attr("cx", d => map.latLngToLayerPoint([d.lat, d.lon]).x)
+ .attr("cy", d => map.latLngToLayerPoint([d.lat, d.lon]).y)
+ .attr("r", 14)
+ .style("fill", "red")
+ .attr("stroke", "red")
+ .attr("stroke-width", 3)
+ .attr("fill-opacity", .4)
 
-  function zoomed(transform) {
-    const tiles = tile(transform);
+ function update() {
+      d3.selectAll("circle")
+        .attr("cx", d => map.latLngToLayerPoint([d.lat, d.lon]).x)
+        .attr("cy", d => map.latLngToLayerPoint([d.lat, d.lon]).y)
+    }
 
-    image = image.data(tiles, d => d).join("image")
-        .attr("xlink:href", d => url(...d3Tile.tileWrap(d)))
-        .attr("x", ([x]) => (x + tiles.translate[0]) * tiles.scale)
-        .attr("y", ([, y]) => (y + tiles.translate[1]) * tiles.scale)
-        .attr("width", tiles.scale)
-        .attr("height", tiles.scale);
-  }
+map.on("moveend", update)
