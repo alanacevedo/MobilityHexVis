e8885c6eb28275787285c8a8766c1712aa702573
Alan Acevedo <alanacevedo@pop-os.localdomain>
2024-02-29 09:26:31 -0300

    data_chile colors

--- displayMap.js
+++ displayMap.js
@@ -1,17 +1,13 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-import * as d3Tile from "https://cdn.jsdelivr.net/npm/d3-tile@1/+esm"
 import * as L from 'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/+esm'
 
-// https://bost.ocks.org/mike/leaflet/
-// https://d3-graph-gallery.com/graph/bubblemap_leaflet_basic.html
-
-import { accessToken, VanAreas } from "./static.js"
+import { accessToken } from "./static.js"
 
 const INITIAL_CENTER = [-33.471258, -70.646552]
 const INITIAL_ZOOM = 11
 const MAX_ZOOM = 12
 
-const map = L.map('map').setView([49.2527, -123.1207], 11.5);
+const map = L.map('map').setView(INITIAL_CENTER, INITIAL_ZOOM);
 
 
 const osmLayer = L.tileLayer(
@@ -28,7 +24,7 @@ const g = svg.append('g').attr('class', 'leaflet-zoom-hide')
 
 // Use Leaflets projection API for drawing svg path (creates a stream of projected points)
 const projectPoint = function(x, y) {
-    const point = map.latLngToLayerPoint(new L.LatLng(y, x))
+    const point = map.latLngToLayerPoint(new L.LatLng(x, y))
     this.stream.point(point.x, point.y)
   }
   
@@ -37,35 +33,27 @@ const projection = d3.geoTransform({point: projectPoint})
 // creates geopath from projected points (SVG)
 const pathCreator = d3.geoPath().projection(projection)
 
-// lonlat
-const link = [[[-123.167556, 49.235430], [-123.068615, 49.245292]]]
+const colorMap = {
+    abc1: "orange",
+    c2: "red",
+    c3: "green",
+    d: "blue",
+    e: "purple"
+}
 
-let areaPaths, line
+let line
 
 // Function to place svg based on zoom
 const initialize = (data) => {
-    areaPaths = g.selectAll('path')
-    .data(VanAreas.features)
-    .join('path')
-    .attr('fill-opacity', 0.3)
-    .attr('stroke', 'black')
-    .attr("z-index", 3000)
-    .attr('stroke-width', 2.5)
-    .attr("class", "leaflet-interactive")
-    .on("mouseover", function(d){
-                d3.select(this).attr("fill", "red")
-            })
-    .on("mouseout", function(d){
-                d3.select(this).attr("fill", "black")
-            })
-    
 
     line = g.selectAll("linePath")
-    .data(link)
+    .data(data)
     .join("path")
     .style("fill", "none")
-    .style("stroke", "orange")
-    .style("stroke-width", 7)
+    .style("stroke", (d) => (
+        colorMap[d.category]
+    ))
+    .style("stroke-width", 3)
 
     map.on('zoomend', update)
     map.on("moveend", update)
@@ -74,14 +62,13 @@ const initialize = (data) => {
 }
 
 const update = () => {
-    areaPaths.attr('d', pathCreator)
-    line.attr("d", function(d) {
-        return pathCreator({
+    line.attr("d", (d) => (
+        pathCreator({
             
             "type": "LineString",
-            "coordinates": d
-        })
-    })
+            "coordinates": [[d.lat_start, d.lon_start], [d.lat_end, d.lon_end]]
+        }))
+    )
   }
 
 d3.csv("/data/trips_by_category.csv", (d) => {
@@ -90,7 +77,7 @@ d3.csv("/data/trips_by_category.csv", (d) => {
 })
 .then((data) => {
     console.log(data)
-    initialize()
+    initialize(data)
 })
 
   // reset whenever map is moved or zoomed
