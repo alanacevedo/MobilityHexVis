4f26d0822f369853caad9ca4bbf7d52e4325e8f2
alanacevedo <michael.acevedo@ug.uchile.cl>
2024-03-14 23:24:38 -0300

    small refactor

--- utils/projectPoint.js
+++ utils/projectPoint.js
@@ -1,9 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import * as L from 'https://cdn.jsdelivr.net/npm/leaflet@1.9.4/+esm'
 
-const line = d3.line()
-    .x(d => d.x)
-    .y(d => d.y)
 
 // projects latlong coordinates to leaflet map coordinates for drawing svg
 // returns object {x: number, y: number}
@@ -16,6 +13,11 @@ function getPathFromLinkData(linkData, LMap) {
     const start = projectPoint(latlon_start, LMap)
     const end = projectPoint(latlon_end, LMap)
 
+    return drawCurvedPath(start, end)
+
+}
+
+function drawCurvedPath(start, end) {
     // Usa curva de Bézier cuadrática, podría usar otras más complicadas.
     // https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
 
@@ -38,7 +40,7 @@ function getPathFromLinkData(linkData, LMap) {
     const arrowLength = 20;
     const arrowStartFraction = 0.3; // Adjust as needed
 
-    // Calculate the adjusted endpoint for the arrowhead
+    // Calculate the adjusted endpoint for the arrowhead, a little before actual edge end
     const adjustedEnd = {
         x: end.x - arrowStartFraction * arrowLength * Math.cos(angle),
         y: end.y - arrowStartFraction * arrowLength * Math.sin(angle)
