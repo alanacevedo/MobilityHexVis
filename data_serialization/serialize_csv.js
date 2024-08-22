05737dcbc2550ebdb99f3b96b7e0024e320e3b7d
alanacevedo <michael.acevedo@ug.uchile.cl>
2024-08-21 23:05:20 -0400

    avance script csv -> protobuf

--- data_serialization/serialize_csv.js
+++ data_serialization/serialize_csv.js
@@ -2,34 +2,38 @@ import fs from 'fs';
 import * as d3 from 'd3';
 import protobuf from 'protobufjs';
 import Long from 'long'
+import csv from 'csv-parser';
+import { parse } from 'csv-parse/sync';
+
+// this script should be run from OD_vis_chile root
+
+const CSV_DIR = "../../data_tesis/output/h3_7"
 
 const serializeCsvToProtobuf = async (start_hour, end_hour) => {
     const root = await protobuf.load("./data_serialization/data.proto")
     const DataEntry = root.lookupType("DataEntry")
     const DataEntries = root.lookupType("DataEntries")
 
-    const payloads = [
-        {
-            h3_O: "wena",
-            h3_D: "chao",
-            group: 1,
-            count: 2,
-            distance: 3.0,
-            normTotal: 32.3,
-            normGroup: 45.0,
-            origin: Long.fromString("88b2c5541bfffff", 16)
-        },
-        {
-            h3_O: "hola",
-            h3_D: "adios",
-            group: 2,
-            count: 3,
-            distance: 4.0,
-            normTotal: 12.3,
-            normGroup: 22.0,
-            origin: Long.fromString("77a2c5541afffff", 16)
-        }
-    ];
+
+    const CSV_PATH = `${CSV_DIR}/od_${start_hour}_${end_hour}_cuartil.csv`
+
+    const fileContent = fs.readFileSync(CSV_PATH, 'utf-8');
+
+    // Parse the CSV content synchronously
+    const records = parse(fileContent, {
+        columns: true,  // Treat the first row as headers and create objects
+        skip_empty_lines: true
+    });
+
+    const payloads = records.map(obj => ({
+        h3_O: Long.fromString(obj.h3_O, 16),
+        h3_D: Long.fromString(obj.h3_D, 16),
+        group: Number(obj.group),
+        count: Number(obj.count),
+        distance: Number(obj.distance),
+        normTotal: Number(obj.norm_total),
+        normGroup: Number(obj.norm_group),
+    }))
 
     const dataEntriesList = payloads.map(payload => {
         const errMsg = DataEntry.verify(payload);
@@ -55,7 +59,11 @@ const serializeCsvToProtobuf = async (start_hour, end_hour) => {
 
     const decoded_message = DataEntries.decode(readBuffer)
     const decoded_object = DataEntries.toObject(decoded_message, { defaults: true })
-    console.log(decoded_object.entries.map(obj => ({ ...obj, origin: obj.origin.toString(16) })))
+    console.log(decoded_object.entries.slice(0, 3).map(obj => ({
+        ...obj,
+        h3_O: obj.h3_O.toString(16),
+        h3_D: obj.h3_D.toString(16)
+    })))
     //console.log(decoded_object.origin.toString(16))
 
 }
